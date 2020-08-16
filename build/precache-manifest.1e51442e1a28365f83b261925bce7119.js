f29b08ea58c017da29d970ce6f5a27d87b159e35
Arif Romdon <arifdop182@gmail.com>
2020-08-16 19:30:53 +0700

    update package

---
+++ build/precache-manifest.1e51442e1a28365f83b261925bce7119.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2863b38d0ecf9a48a6b0c099f6f7a99",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c3091104a27b6d1e18bd",
+    "url": "/static/css/3.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "dab537e9b58bdd7a232a",
+    "url": "/static/css/main.d11963bf.chunk.css"
+  },
+  {
+    "revision": "74fa002b9076891fb773",
+    "url": "/static/js/0.ab7d28e9.chunk.js"
+  },
+  {
+    "revision": "c3091104a27b6d1e18bd",
+    "url": "/static/js/3.919c8143.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/3.919c8143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b6ed94bdd72585af0d",
+    "url": "/static/js/4.ba88d057.chunk.js"
+  },
+  {
+    "revision": "cc63abf8160f44a95162",
+    "url": "/static/js/5.932c13df.chunk.js"
+  },
+  {
+    "revision": "2ede6dff55b1114805d3",
+    "url": "/static/js/6.0cc3478d.chunk.js"
+  },
+  {
+    "revision": "dab537e9b58bdd7a232a",
+    "url": "/static/js/main.a50d6b57.chunk.js"
+  },
+  {
+    "revision": "13c9bf5e968a50329105",
+    "url": "/static/js/runtime-main.3b46d438.js"
+  },
+  {
+    "revision": "2505bfbd9bde14a7829cc8c242a0d25c",
+    "url": "/static/media/Ubuntu-Regular.2505bfbd.ttf"
+  },
+  {
+    "revision": "83b2d656509f81f6bd977db213ee679f",
+    "url": "/static/media/ic-build.83b2d656.png"
+  },
+  {
+    "revision": "1842b3224b5abe917894fe1c9059e580",
+    "url": "/static/media/ic-group.1842b322.png"
+  },
+  {
+    "revision": "ecc6f5d20f13e82924a4e612d440b388",
+    "url": "/static/media/ic-hand.ecc6f5d2.png"
+  },
+  {
+    "revision": "20db9c38f115c650216b321584b0107d",
+    "url": "/static/media/ic-money.20db9c38.png"
+  }
+]);
